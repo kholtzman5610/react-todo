d757c9e47c55883068e22bf6a42234bbd709e1cb
kholtzman560 <kassidy.holtzman@outlook.com>
2024-07-23 13:28:21 -0400

    Completed lesson_1_5

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,32 +1,25 @@
 import * as React from 'react';
 
-function AddTodoForm({ onAddTodo }) {
+function AddTodoForm(props) {
   const [todoTitle, setTodoTitle] = React.useState('');
-
-  const handleAddTodo = (event) => {
-    event.preventDefault();
-    const newTodo = {
-      title: todoTitle,
-      id: Date.now(),
-    };
-    onAddTodo(newTodo);
-    setTodoTitle('');
-  };
 
   const handleTitleChange = (event) => {
     setTodoTitle(event.target.value);
   };
 
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    props.onAddTodo({
+      title: todoTitle,
+      id: Date.now(),
+    });
+    setTodoTitle('');
+  };
+
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="title">Title</label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        value={todoTitle}
-        onChange={handleTitleChange}
-      />
+      <input type="text" id="title" name="title" value={todoTitle} onChange={handleTitleChange} />
       <button type="submit">Add</button>
     </form>
   );
