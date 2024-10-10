a3945562a7398498af80b7ea11e76374da1687f7
kholtzman560 <kassidy.holtzman@outlook.com>
2024-10-10 13:27:44 -0400

    Hotfix Sass Fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Example: custom options for Sass
-        additionalData: `@import "src/styles/variables.scss";` // if you have global variables
-      },
-    },
-  },
-});
+})
